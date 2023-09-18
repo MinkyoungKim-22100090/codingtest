6b0412cf9789f3933df164724521f0b19d7310da
MinkyoungKim-22100090 <100542891+MinkyoungKim-22100090@users.noreply.github.com>
2023-09-18 20:53:00 +0900

    Create 10808.c

---
+++ 10808/10808.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int arr[26]={0};
+  char word[100];
+
+  scanf("%s",word);
+
+  for(int i=0; i<26; i++) arr[word[i] - 'a']++;
+  for(int i=0; i<26; i++) printf("%d",arr[i]);
+  
+  return 0;
+}
